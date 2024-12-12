ab4e5b579e58038d43c97508a49cf48765fc449f
JustNDung <nguyenngocdung19122005@gmail.com>
2024-12-12 18:33:35 +0700

    Update underground camera

--- Assets/Scripts/SlideScrolling.cs
+++ Assets/Scripts/SlideScrolling.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class SlideScrolling : MonoBehaviour
 {
     private Transform player;
+    public float height = 2f;
+    public float undergroundHeight = -24f;
 
     private void Awake()
     {
@@ -15,6 +17,13 @@ public class SlideScrolling : MonoBehaviour
         cameraPos.x = Mathf.Max(cameraPos.x, player.position.x);
         //cameraPos.x = player.position.x;
         transform.position = cameraPos;
-    } 
+    }
+
+    public void SetUnderground(bool underground)
+    {
+        Vector3 cameraPos = transform.position;
+        cameraPos.y = underground ? undergroundHeight : height;
+        transform.position = cameraPos;
+    }
     
 }
